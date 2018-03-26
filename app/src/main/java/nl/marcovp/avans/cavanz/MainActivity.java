27c13f1f38afbfb1ed90a7c894438ca89383e137
Marco van Poortvliet <Marc0tjevp@gmail.com>
2018-03-26 16:22:34 +0200

    Basic project structure and navigation bar

---
+++ app/src/main/java/nl/marcovp/avans/cavanz/MainActivity.java
@@ -0,0 +1,44 @@
+package nl.marcovp.avans.cavanz;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    private TextView mTextMessage;
+
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
+            = new BottomNavigationView.OnNavigationItemSelectedListener() {
+
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            switch (item.getItemId()) {
+                case R.id.navigation_movies:
+                    mTextMessage.setText(R.string.text_navbar_films);
+                    return true;
+                case R.id.navigation_tickets:
+                    mTextMessage.setText(R.string.text_navbar_tickets);
+                    return true;
+                case R.id.navigation_info:
+                    mTextMessage.setText(R.string.text_navbar_info);
+                    return true;
+            }
+            return false;
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mTextMessage = (TextView) findViewById(R.id.message);
+        BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+    }
+
+}
