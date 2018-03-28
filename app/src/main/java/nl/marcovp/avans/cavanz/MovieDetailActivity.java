ed355d85ea1e1fed74b37aec57281954e12f9a49
Djim <oomesdjim@gmail.com>
2018-03-29 00:19:37 +0200

    Sync

---
+++ app/src/main/java/nl/marcovp/avans/cavanz/MovieDetailActivity.java
@@ -0,0 +1,24 @@
+package nl.marcovp.avans.cavanz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+
+/**
+ * Created by Djim on 28-3-2018.
+ */
+
+public class MovieDetailActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_movie_detail);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
